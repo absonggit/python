633e8c39ea2effc416df2ea1736cd25eeec298f4
ABsong <466054646@qq.com>
2019-05-03 12:59:35 +0800

    asd

--- replace.py
+++ replace.py
@@ -1,5 +1,6 @@
 import sys
-
+if len(sys.argv) != 3:
+    exit("Parameters of the lack of！At a minimum, enter the string that needs to be replaced and changed")
 find_str = sys.argv[1]
 replace_str = sys.argv[2]
 with open("old.txt", "r", encoding="utf-8") as f1, open("new.txt", "w", encoding="utf-8") as f2:
