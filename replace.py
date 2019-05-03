2c4bb8ce4dcdb771a41afb59ea0653ac7e6de971
ABsong <466054646@qq.com>
2019-05-03 13:02:46 +0800

    asd

--- replace.py
+++ replace.py
@@ -3,8 +3,8 @@ if len(sys.argv) != 3:
     exit("Parameters of the lack of！At a minimum, enter the string that needs to be replaced and changed")
 find_str = sys.argv[1]
 replace_str = sys.argv[2]
-with open("old.txt", "r", encoding="utf-8") as f1, open("new.txt", "w", encoding="utf-8") as f2:
-    for line in f1:
+with open("file.txt", "r+", encoding="utf-8") as f:
+    for line in f:
         if find_str in line:
             line = line.replace(find_str, replace_str)
-        f2.write(line)
+        f.write(line)
