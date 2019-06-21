a8b435b4106115709608aeff1e3320686ba057d3
Absong <absong715@gmail.com>
2019-06-21 13:26:23 +0800

    修改 jenkins 批量修改job配置脚本

--- 批量修改jenkinsJob配置.py
+++ 批量修改jenkinsJob配置.py
@@ -8,25 +8,24 @@ class ChangeData(object):
         self.value = value
         self.xml_tag = xml_tag
 
-
-def find_file(s, *end_string):
-    array = map(s.endswith, end_string)
-    if True in array:
-        return True
-    else:
-        return False
+    def find_file(self, *end_string):
+        array = map(self.endswith, end_string)
+        if True in array:
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
     data = ChangeData
     data.xml_tag = "numToKeep"
     data.path = "./"
-    data.value = "30"
-    s = os.listdir(data.path)
+    data.value = "50"
+    file_path = os.listdir(data.path)
     f_file = []
-    for i in s:
-        if find_file(i, '.xml'):
-            f_file.append(os.path.join(data.path, i))
+    for file_name in file_path:
+        if data.find_file(file_name, '.xml'):
+            f_file.append(os.path.join(data.path, file_name))
     for file in f_file:
         tree = ET.parse(file)
         root = tree.getroot()
@@ -35,6 +34,5 @@ if __name__ == '__main__':
             old_text = i.text
             i.text = data.value
             print(
-                f"将文件\033[31m{file}\033[0m\n\t\033[32m{i.tag}\033[0m标签的值\033[33m{old_text}\033[0m修改为\033[34m{i.text}\033[0m")
+                f"<INFO> 将文件\033[31m【{file}】\033[0m中\033[32m【标签：{i.tag}】\033[0m的值\033[33m【{old_text}】\033[0m修改为\033[34m【{i.text}】\033[0m")
         tree.write(file, encoding='UTF-8')
-
